609f5496e7efb6c9ec1734325fc1ba12bff43b48
sambaiz <godgourd@gmail.com>
2021-06-03 10:25:19 +0900

    add sam_version parameters to install command

--- src/scripts/install.sh
+++ src/scripts/install.sh
@@ -2,7 +2,7 @@ cd /tmp || true
 if [[ $EUID == 0 ]]; then export SUDO=""; else # Check if we are root
   export SUDO="sudo";
 fi
-curl -L https://github.com/aws/aws-sam-cli/releases/latest/download/aws-sam-cli-linux-x86_64.zip -o aws-sam-cli-linux-x86_64.zip
+curl -L https://github.com/aws/aws-sam-cli/releases/$SAM_VERSION/download/aws-sam-cli-linux-x86_64.zip -o aws-sam-cli-linux-x86_64.zip
 unzip aws-sam-cli-linux-x86_64.zip -d sam-installation
 $SUDO ./sam-installation/install
 which sam
